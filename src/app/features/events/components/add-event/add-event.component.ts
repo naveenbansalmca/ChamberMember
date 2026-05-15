adff91dd2a88993ea7ab43a783821c1fa189cd45
Naveen <naveenbansal56@gmail.com>
2026-05-15 13:42:18 +0530

    a

--- src/app/features/events/components/add-event/add-event.component.ts
+++ src/app/features/events/components/add-event/add-event.component.ts
@@ -3,23 +3,200 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { RequiredDirective } from "../../../../shared/styles/directive/required.directive";
+import {
+  ClassicEditor,
+  Essentials,
+  Paragraph,
+  Bold,
+  Italic,
+  Underline,
+  Strikethrough,
+  Heading,
+  List,
+  Link,
+  Image,
+  ImageToolbar,
+  ImageCaption,
+  ImageStyle,
+  ImageResize,
+  ImageUpload,
+  Table,
+  TableToolbar,
+  BlockQuote,
+  Alignment,
+  Font,
+  Indent,
+  IndentBlock,
+  Code,
+  CodeBlock,
+  HorizontalLine,
+  MediaEmbed,
+  PasteFromOffice,
+  FontBackgroundColor,
+  FontColor,
+  RemoveFormat,
+  Subscript,
+  Superscript,
+  Undo
+} from 'ckeditor5';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+
 
 @Component({
   selector: 'app-add-event',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RequiredDirective],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RequiredDirective, CKEditorModule],
   templateUrl: './add-event.component.html',
-  styleUrl: './add-event.component.css',
+  styleUrls: ['./add-event.component.css'],
 })
 export class AddEventComponent implements OnInit {
   eventForm!: FormGroup;
   additionalPhotos: File[] = [];
 
-  constructor(private fb: FormBuilder, private router: Router) {}
+  public Editor = ClassicEditor;
+
+  content = '';
+
+  config = {
+
+    licenseKey: 'GPL',
+
+    plugins: [
+      Essentials,
+      Paragraph,
+      Bold,
+      Italic,
+      Underline,
+      Strikethrough,
+      Subscript,
+      Superscript,
+
+      Heading,
+
+      Font,
+      FontColor,
+      FontBackgroundColor,
+
+      Alignment,
+
+      List,
+
+      Indent,
+      IndentBlock,
+
+      Link,
+
+      Image,
+      ImageToolbar,
+      ImageCaption,
+      ImageStyle,
+      ImageResize,
+      ImageUpload,
+
+      Table,
+      TableToolbar,
+
+      BlockQuote,
+
+      HorizontalLine,
+
+      Code,
+      CodeBlock,
+
+      MediaEmbed,
+
+      PasteFromOffice,
+
+      RemoveFormat,
+
+      Undo
+    ],
+
+    toolbar:
+    {
+      shouldNotGroupWhenFull: true,
+
+      items: [
+
+        'undo',
+        'redo',
+
+        '|',
+
+        'removeFormat',
+
+        '|',
+
+        'heading',
+
+        '|',
+
+        'fontFamily',
+        'fontSize',
+        'fontColor',
+        'fontBackgroundColor',
+
+        '|',
+
+        'bold',
+        'italic',
+        'underline',
+        'strikethrough',
+
+        '|',
+
+        'subscript',
+        'superscript',
+
+        '|',
+
+        'alignment',
+
+        '|',
+
+        'bulletedList',
+        'numberedList',
+
+        '|',
+
+        'outdent',
+        'indent',
+
+        '|',
+
+        'link',
+        'uploadImage',
+        'insertTable',
+        'mediaEmbed',
+
+        '|',
+
+        'blockQuote',
+
+        '|',
+
+        'code',
+        'codeBlock',
+
+        '|',
+
+        'horizontalLine'
+      ]
+    }
+  };
+
+
+
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
+
+
+
+
     this.initializeForm();
   }
+
 
   initializeForm(): void {
     this.eventForm = this.fb.group({
